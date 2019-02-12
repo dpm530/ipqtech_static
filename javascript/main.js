9417f0907bf2697d308cf28d6b16d25244c1b90d
Daniel Palacios <danielpalacios530@gmail.com>
2019-02-12 14:51:52 -0600

    fixed bug for mibile devices

--- javascript/main.js
+++ javascript/main.js
@@ -1,10 +1,12 @@
-
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-    document.getElementById("header_navbar").style.backgroundColor = "rgba(0, 0, 0, 0.5)";
-  } else {
-    document.getElementById("header_navbar").style.backgroundColor = "rgba(0, 0, 0, 0)";
-  }
-}
+//
+// window.onscroll = function() {scrollFunction()};
+//
+// function scrollFunction() {
+//    if (document.body.screen.width > 400) {
+//       if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+//          document.getElementById("header_navbar").style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+//       } else {
+//          document.getElementById("header_navbar").style.backgroundColor = "rgba(0, 0, 0, 0)";
+//       }
+//    }
+// }
