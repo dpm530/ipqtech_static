600323706a897b67ff81cef37a218ee0ae52be3a
WebProFe <medina.techie@gmail.com>
2019-03-01 15:35:37 -0600

    services mobile

--- javascript/main.js
+++ javascript/main.js
@@ -71,18 +71,42 @@
 //   }
 // }
 
+//////////TYPEWRITER EFFECT///////
+
 var app = document.getElementById('app');
 
 var typewriter = new Typewriter(app, {
     loop: true
 });
 
-typewriter.typeString('Hello World!')
-    .pauseFor(2500)
-    .deleteAll()
-    .typeString('Strings can be removed')
-    .pauseFor(2500)
-    .deleteChars(7)
-    .typeString('<strong>altered!</strong>')
-    .pauseFor(2500)
-    .start();
+typewriter.typeString('Custom Websites')
+   .pauseFor(2500)
+   .deleteAll()
+   .typeString('E-commerce Websites')
+   .pauseFor(2500)
+   .deleteAll()
+   .typeString('Cloud Solutions')
+   .pauseFor(2500)
+   .start();
+
+    ////////SMOTH SCROLL EFFECT////////
+
+    $(document).on('click', 'a[href^="#"]', function(e) {
+      // target element id
+      var id = $(this).attr('href');
+      
+      // target element
+      var $id = $(id);
+      if ($id.length === 0) {
+          return;
+      }
+      
+      // prevent standard hash navigation (avoid blinking in IE)
+      e.preventDefault();
+      
+      // top position relative to the document
+      var pos = $id.offset().top;
+      
+      // animated top scrolling
+      $('body, html').animate({scrollTop: pos});
+    });  
